201669e13b81abf6e815d88e0acc322d2dee268d
Guilherme Dos Santos <guil_stay@hotmail.com>
2018-11-23 19:47:10 -0200

    URL API

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
 //export const URL ="http://localhost:8080/"
-export const URL ="https://ntm-projectbackend.herokuapp.com/"
+//export const URL ="https://ntm-projectbackend.herokuapp.com/"
+export const URL ="http://35.198.9.57:8080/"
